72841a427b891e634175cf660999792aa5b2c3da
Traun Leyden <traun.leyden@gmail.com>
2019-01-03 17:00:07 -0800

    Remove unused code, just testing “patch via gerrit”
    Change-Id: I96c0e537441ec8372110c305f487f195bf64d236
    Reviewed-on: http://review.couchbase.org/103270
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Traun Leyden <traun.leyden@gmail.com>
    Tested-by: Traun Leyden <traun.leyden@gmail.com>

--- utils.go
+++ utils.go
@@ -2,76 +2,8 @@ package mobile_service
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
-
-	"github.com/couchbase/cbauth"
-	"github.com/couchbase/gocb"
 )
-
-// CBAuthURL rewrites a URL with credentials, for use in a cbauth'ed
-// environment.
-func CBAuthURL(urlStr string) (string, error) {
-	u, err := url.Parse(urlStr)
-	if err != nil {
-		return "", err
-	}
-
-	cbUser, cbPasswd, err := cbauth.GetHTTPServiceAuth(u.Host)
-	if err != nil {
-		return "", err
-	}
-
-	u.User = url.UserPassword(cbUser, cbPasswd)
-
-	return u.String(), nil
-}
-
-func CBAuthURL2(urlStr string) (string, error) {
-	u, err := url.Parse(urlStr)
-	if err != nil {
-		return "", err
-	}
-
-	cbUser, cbPasswd, err := cbauth.GetMemcachedServiceAuth(u.Host)
-	if err != nil {
-		return "", err
-	}
-
-	u.User = url.UserPassword(cbUser, cbPasswd)
-
-	return u.String(), nil
-}
-
-func GetCBAuthMemcachedCreds(urlStr string) (username, password string, err error) {
-
-	u, err := url.Parse(urlStr)
-	if err != nil {
-		return "", "", err
-	}
-
-	return cbauth.GetMemcachedServiceAuth(u.Host)
-
-}
-
-func OpenBucket(bucketName, connSpec string) (bucket *gocb.Bucket, err error) {
-
-	cluster, err := gocb.Connect(connSpec)
-
-	username, password, err := GetCBAuthMemcachedCreds(connSpec)
-	if err != nil {
-		return nil, err
-	}
-
-	authenticator := gocb.PasswordAuthenticator{
-		Username: username,
-		Password: password,
-	}
-	cluster.Authenticate(authenticator)
-
-	return cluster.OpenBucket(bucketName, "")
-
-}
 
 func AddTrailingSlash(initial string) string {
 	if strings.HasSuffix(initial, "/") {
